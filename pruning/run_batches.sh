710802eade24de6aa658cabb50eb927a88a6e16b
Matthias H Hennig <mhhennig@gmail.com>
2021-06-08 20:29:01 +0100

    new expts

--- pruning/run_batches.sh
+++ pruning/run_batches.sh
@@ -4,7 +4,7 @@
 # export KMP_SETTINGS=TRUE
 # export KMP_AFFINITY=granularity=fine,compact
 
-working_file=working3.sh
+working_file=working4.sh
 
 echo > $working_file
 
@@ -12,8 +12,8 @@ echo > $working_file
 for i in $(seq 1 8)
 do
 #   echo python MNIST_PruneDBM_HeuristicFI.py 20 50 6 2 $i >> working.sh
-   echo python MNIST_PruneDBM_HeuristicFI.py 10 25 12 $i 10 --no-constant >> $working_file 
-#  echo python MNIST_PruneDBM_VarianceFI.py 10 25 12 $i 10 --no-constant >> working2.sh
+#   echo python MNIST_PruneDBM_HeuristicFI.py 10 25 12 $i 10 --no-constant >> $working_file 
+  echo python MNIST_PruneDBM_VarianceFI.py 10 25 12 $i 10 --no-constant >> $working_file
 ##  echo python MNIST_PruneDBM_VarianceFI.py 5 25 12 $i 10 --no-constant >> working.sh
 #   echo python MNIST_PruneDBM_VarianceFI_allzero.py 10 25 6 $i 10 >> working.sh
 #   echo python MNIST_PruneDBM_W.py 10 25 12 $i 10 >> working3.sh
